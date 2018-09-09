af58bd7f29f3c87d686552bb37667f180e9a42fd
stevenpack <steven.pack.code@gmail.com>
2018-09-09 11:45:13 -0700

    api with tests. ready for wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-// #![feature(extern_prelude)]
-extern crate rand;
+#![feature(test)]
 
-mod phrases {
+extern crate rand;
+extern crate test;
+
+pub mod phrases {
     use rand::{thread_rng, Rng};
     // Compiled by http://www.bobrosslipsum.com/ 2016 January
     static PHRASES: [&str; 4] = [  
         "A beautiful little sunset.",
         "A big strong tree needs big strong roots.",
-        "A fan brush is a fantastic piece of equipment. Use it. Make friends with it.",
+        "A fan brush is a fantastic piece of equipment.",
         "A happy cloud."];
 
     fn get_random_phrase() -> &'static str {        
@@ -17,57 +19,48 @@ mod phrases {
     }
 
     fn get_phrases(len: usize) -> Vec<&'static str> {
-        let mut phrases = Vec::with_capacity(len);
-        for _ in 0..len {
-            phrases.push(get_random_phrase())
-        }
-        phrases
+        (0..len)
+            .map(|_| get_random_phrase())
+            .collect()        
     }
 
-    fn break_lines(phrases: Vec<&'static str>, new_line: usize) -> Vec<&'static str> {
-        let len = phrases.len();
-        let new_lines = len / new_line;
-        let capacity = len + new_lines;
-        let mut phrases_with_newlines = Vec::with_capacity(capacity);
-        for i in 0..len {
-            if i > 0 && i % new_line == 0 {
-                phrases_with_newlines.push("\n\n");
-            } 
-            //intention here is not to resize the vec, so check our math is right
-            debug_assert!(phrases_with_newlines.len() < capacity);
-
-            phrases_with_newlines.push(phrases[i]);            
-        }
-        phrases_with_newlines
+    fn need_newline(newline: usize, idx: usize) -> bool {
+        //idx+1 because idx is zero-based but we want a new line after "every x phrases".
+        (newline > 0) && (idx > 0) && ((idx + 1) % newline == 0)
     }
 
-    pub fn get_phrases_with_newlines(len: usize, new_line: usize) -> Vec<&'static str> {
-        let phrases = get_phrases(len);
-        break_lines(phrases, new_line)
+    fn need_space(newline: usize, idx: usize) -> bool {
+        !need_newline(newline, idx)
     }
-
-    pub fn get_phrase_text(phrases: usize, new_line: usize) -> String {
+    
+    pub fn get_phrase_text(phrase_cnt: usize, newline: usize) -> String {
         
-        let phrases_vec = get_phrases(phrases);
+        let phrases_vec = get_phrases(phrase_cnt);
         let mut string = String::new();
         for i in 0..phrases_vec.len() {
-            if i > 0 && new_line > 0 && i % new_line == 0 {
+
+            println!("{}", string.capacity());
+
+            //the phrase
+            string.push_str(phrases_vec[i]);
+
+            //spaces between phrases
+            if need_space(newline, i) {
+                string.push(' ');
+            }
+
+            //new lines
+            if need_newline(newline, i) {
                 string.push_str("\n\n");
             }
-            string.push_str(phrases_vec[i]);
         }
         string
     }
-//     let happyPhrases = new Array(length);
-//     for (let i=0; i<length; i++) {
-//         happyPhrases[i] = getPhrase() + " ";
-//     }
-//     return happyPhrases;
-// }
 
     #[cfg(test)]
     mod tests {
         use super::*;
+        use test::Bencher;
 
         #[test]
         fn get_random() {
@@ -83,37 +76,36 @@ mod phrases {
         }
 
         #[test]
-        fn get_phrases_with_newlines_counts() {
-            let phrases = get_phrases_with_newlines(101, 5);
-            
-            let new_line_cnt = phrases.iter().filter(|&n| *n == "\n\n").count();
-            let phrase_cnt = phrases.len() - new_line_cnt;
-            assert_eq!(new_line_cnt, 20);
-            assert_eq!(phrase_cnt, 101);
+        fn is_newline_tests() {
+            assert!(!need_newline(5, 3));
+            assert!(need_newline(5, 4)); //idx of 4 = 5 phrases (0 based)
+            assert!(!need_newline(5, 5));
+            assert!(!need_newline(5, 6));
+        }
 
-            println!("{:?}", phrases);
+         #[test]
+        fn is_space_tests() {
+            assert!(need_space(5, 3));
+            assert!(!need_space(5, 4));
+            assert!(need_space(5, 5));
+            assert!(need_space(5, 6));
         }
 
          #[test]
         fn get_phrase_text_dump() {
-            let string = get_phrase_text(101, 5);
+            let string = get_phrase_text(16, 5);
             println!("{:?}", string);
+        }
+
+        #[bench]
+        fn bench_get_phrases(b: &mut Bencher) {
+            b.iter(|| get_phrases(1000));
+        }
+        
+        #[bench]
+        fn bench_get_phrase_text(b: &mut Bencher) {
+            b.iter(|| println!("{}", get_phrase_text(1000, 5)))
         }
     }
 
 }
-
-
-
-
-// function breakLines(phraseArr, newLine) {
-//     let lines = [];
-//     for (let i=0; i < phraseArr.length; i++) {
-//         lines.push(phraseArr[i]);
-//         if (i > 0 && i % newLine === 0) {
-//             lines.push("\n\n");
-//         }
-//     }
-//     return lines;
-// }
-
